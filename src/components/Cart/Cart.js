c990b0e7e0909ef75a6ca523026992b4cfbfdf0e
Rokibulhasan360 <rokibulhasan360@gmail.com>
2020-04-21 01:42:01 +0600

    done with ema-john

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 
 const Cart = (props) => {
@@ -42,9 +42,9 @@ const formatNumber= num=>{
     <p><small>Tax+Vat: {tax}</small></p>
     <p>Total: {grandTotal.toFixed(2)}</p>
     <br/>
-    <Link to="/review">
-    <button className="main-button">Review Order</button>
-    </Link>
+    {
+        props.children
+    }
         </div>
     );
 };
